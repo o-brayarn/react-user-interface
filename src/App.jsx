d65c603cbaec750a8398cb618c2e93c916e3d1ea
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-07-23 17:54:51 +0300

    update margin

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import AddAppointment from "./components/AddAppointments";
 function App() {
   return (
     <div className="container mx-auto mt-5 font-thin">
-      <h1 className="text-5xl">
+      <h1 className="text-5xl mb-5">
         {" "}
         <BiCalendar className="inline-block text-red-400 align-top" /> Your
         Appointments
