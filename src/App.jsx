a1d2474a9423675e8ecc7753d2ef88944b468d5e
Brayarn Ogeda <brayarnogeda@gmail.com>
2024-07-25 16:52:57 +0300

    useEffect and useCallback hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { BiCalendar } from "react-icons/bi";
 import Search from "./components/Search";
 import AddAppointment from "./components/AddAppointments";
-import appointmentList from "./data.json";
 import AppointmentInfo from "./components/AppointmentInfo";
+import { useState, useCallback, useEffect } from "react";
 
 function App() {
+  const [appointmentList, setAppointmentList] = useState([]);
+
+  const fetchData = useCallback(() => {
+    fetch("./data.json")
+      .then((res) => res.json())
+      .then(setAppointmentList);
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
   return (
     <div className="container mx-auto mt-5 font-thin">
       <h1 className="text-5xl mb-5">
